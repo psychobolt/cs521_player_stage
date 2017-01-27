6565daf72cec2b415300aa2ebe6c86ab88e04024
=psychobolt <psychobolt@gmail.com>
2017-01-26 19:45:34 -0800

    Create proj2 main code

--- player/examples/libplayerc++/proj2.cc
+++ player/examples/libplayerc++/proj2.cc
@@ -1,15 +1,55 @@
 #include <stdio.h>
 #include <libplayerc++/playerc++.h>
 
+class IModule
+{
+  public:
+    virtual ~IModule() {}
+    virtual void run() = 0;
+};
+
+class ActModule : public IModule
+{
+  public:
+    void run() {
+
+    }
+};
+
+class Navigator
+{
+
+};
+
+class Pilot
+{
+
+};
+
+class PlanModule : public IModule
+{
+    ActModule * act;
+  public:
+    PlanModule(ActModule *actModule) {
+      act = actModule;
+    }
+    void run() {
+
+    }
+};
+
 int main(int argc, char *argv[])
 {
-    using namespace PlayerCc;
-    PlayerClient robot("localhost");
+  using namespace PlayerCc;
+  PlayerClient robot("localhost");
+  Position2dProxy p2dProxy(&robot,0);
 
-    Position2dProxy p2dProxy(&robot,0);
+  IModule * act = new ActModule();
+  //IModule * planner = new PlanModule(static_cast<ActModule*>(act));
 
-    while (true) {
-      /* code */
-    }
-    return 0;
+  while (true) {
+    //planner->run();
+    act->run();
+  }
+  return 0;
 }
