8b318d837ea70bbb9ce7783cba9456a0515062d4
psychobolt <psychobolt@gmail.com>
2017-01-28 23:52:08 -0800

    Proj2

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ My workspace for a Robotics course
 
 ## Setup
 
+> For best compatibility use Player 3.1 and Stage 4.1.1
+
 ### Install Ubuntu Desktop LTS
 
 Get the latest [Ubuntu](https://www.ubuntu.com/) Desktop LTS. (Last tested under Ubuntu Desktop 16.04 LTS)
@@ -14,16 +16,24 @@ The install .sh scripts are in /Bash_Files:
 
 1. Download and copy the scripts to a new directory (e.g. /home/user/player_stage)
 2. Execute install_dependency_packages.sh
-3. Execute install_player_stage.sh (Downloads and installs the latest from version control)
+3. Execute install_player_stage.sh
 
 ### Clone Git Repo
 
 > This repository consists of symbolic linked files so most examples will be provided from the player and stage installations. Some example files are added in place their originals, so one can run .cfg files with a compiled Player driver.
 
-## Compiling Example Drivers
+## Drivers
+
+### Compiling Examples
 
 Use the following:
 
 g++ -o proj.o \`pkg-config --cflags playerc++\` proj.cc \`pkg-config --libs playerc++\`
 
 > The git repo ignores files with ".o" extension, therefore you should postfix output files with ".o".
+
+### Development
+
+Primarily drivers are written in C++ and use [libplayerc++](http://playerstage.sourceforge.net/doc/Player-svn/player/group__player__clientlib__cplusplus.html) libraries
+
+> See this [link](http://player-stage-manual.readthedocs.io/) for the online guide to Player/Stage latest builds
--- player/examples/libplayerc++/proj2.cc
+++ player/examples/libplayerc++/proj2.cc
@@ -1,55 +1,223 @@
+#define BOOST_SIGNALS_NO_DEPRECATION_WARNING
+
+#include <cmath>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <libplayerc++/playerc++.h>
+#include "Eigen/Dense"
+#include "easyloggingpp-8.91/easylogging++.h"
+_INITIALIZE_EASYLOGGINGPP
 
+using namespace Eigen;
+using namespace std;
+using namespace PlayerCc;
+
+template <class T>
 class IModule
 {
   public:
+    static T& GetInstance()
+    {
+      static T instance;
+      return instance;
+    }
+    void operator=(T const&);
+    virtual void Run() = 0;
+  protected:
+    IModule() {}
+    IModule(IModule const&);
     virtual ~IModule() {}
-    virtual void run() = 0;
 };
 
-class ActModule : public IModule
+PlayerClient * robot;
+Position2dProxy * position2d;
+ifstream * channel;
+
+class PlanModule : public IModule<PlanModule>
 {
+  friend class IModule;
+  friend class ActModule;
+
   public:
-    void run() {
-
+    void Run()
+    {
+        robot->Read();
+        Navigator::GetInstance().Run();
+        Pilot::GetInstance().Run();
     }
+    bool IsDone()
+    {
+      return Navigator::GetInstance().IsDone();
+    }
+  private:
+    PlanModule() : IModule()
+    {
+      LINFO << "Plan Module initialized";
+    }
+
+    class Pilot : public IModule<Pilot>
+    {
+      friend class IModule;
+
+      public:
+        void Run()
+        {
+           if (IsDone())
+           {
+             forwardSpeed = 0.0;
+             yawSpeed = 0.0;
+             return;
+           }
+           Vector2d target = Navigator::GetInstance().GetNextPos()
+            - GetCurrentPos();
+           double forwardAngle = position2d->GetYaw();
+           double targetAngle = atan2(target[1], target[0]);
+           double deltaAngle = RTOD(NORMALIZE(targetAngle - forwardAngle));
+           int sign = deltaAngle > 0 ? 1 : -1;
+           if (abs(deltaAngle) > 10.0)
+           {
+             forwardSpeed = 0.1;
+             yawSpeed = 2.0 * sign;
+           }
+           else
+           {
+             forwardSpeed = 1.0;
+             yawSpeed = 0.1 * sign;
+           }
+        }
+        double GetForwardSpeed()
+        {
+          return forwardSpeed;
+        }
+        double GetForwardYawSpeed()
+        {
+          return yawSpeed;
+        }
+        Vector2d GetCurrentPos()
+        {
+          Vector2d v(position2d->GetXPos(), position2d->GetYPos());
+          return v;
+        }
+        bool IsDone()
+        {
+          return 0.05 > (Navigator::GetInstance().GetNextPos()
+            - GetCurrentPos()).norm();
+        }
+      private:
+        Pilot() : IModule()
+        {
+          LINFO << "| Pilot Module initialized";
+        }
+
+        double forwardSpeed, yawSpeed;
+        double GetNextYawSpeed(double deltaAngle, double turnRate)
+        {
+          double threshold = 1;
+          if (deltaAngle < -threshold)
+          {
+            return -turnRate;
+          }
+          else if (deltaAngle > threshold)
+          {
+            return turnRate;
+          }
+        }
+    };
+
+    class Navigator : public IModule<Navigator>
+    {
+      friend class IModule;
+
+      public:
+        void Run()
+        {
+          if (Pilot::GetInstance().IsDone() && !IsDone())
+          {
+            string line;
+            if (getline(*channel, line))
+            {
+              istringstream is(line);
+              is >> next[0];
+              is >> next[1];
+              LINFO << "Move to " << next[0] << ", " << next[1];
+            }
+            else
+            {
+              channel->close();
+            }
+          }
+        }
+        Vector2d GetNextPos()
+        {
+          return next;
+        }
+        bool IsDone()
+        {
+          return !channel->is_open();
+        }
+      private:
+        Navigator() : IModule()
+        {
+          robot->Read();
+          next = Pilot::GetInstance().GetCurrentPos();
+          LINFO << "| Navigator Module initialized";
+        }
+        Vector2d next;
+    };
 };
 
-class Navigator
+class ActModule : public IModule<ActModule>
 {
+  friend class IModule;
 
-};
-
-class Pilot
-{
-
-};
-
-class PlanModule : public IModule
-{
-    ActModule * act;
   public:
-    PlanModule(ActModule *actModule) {
-      act = actModule;
+    void Run()
+    {
+      if (position2d != NULL)
+      {
+        position2d->SetSpeed(
+          PlanModule::Pilot::GetInstance().GetForwardSpeed(),
+          PlanModule::Pilot::GetInstance().GetForwardYawSpeed()
+        );
+        // Vector2d target = PlanModule::Navigator::GetInstance().GetNextPos();
+        // position2d->GoTo(target[0], target[1], 0.0);
+      }
     }
-    void run() {
-
+  private:
+    ActModule() : IModule()
+    {
+      LINFO << "Act Module initialized";
     }
 };
 
 int main(int argc, char *argv[])
 {
-  using namespace PlayerCc;
-  PlayerClient robot("localhost");
-  Position2dProxy p2dProxy(&robot,0);
-
-  IModule * act = new ActModule();
-  //IModule * planner = new PlanModule(static_cast<ActModule*>(act));
-
-  while (true) {
-    //planner->run();
-    act->run();
+  if (argc < 2)
+  {
+    printf("Usage: \"%s ./waypoints.txt\" # Please enter a waypoint datafile\n",
+      argv[0]);
+    return -1;
   }
+
+  ifstream file(argv[1]);
+  channel = &file;
+
+  PlayerClient client("localhost");
+  Position2dProxy p2dProxy(&client, 0);
+  robot = &client;
+  position2d = &p2dProxy;
+
+  while (!PlanModule::GetInstance().IsDone())
+  {
+    PlanModule::GetInstance().Run();
+    ActModule::GetInstance().Run();
+
+    usleep(1); // wait 1 sec per Tick
+  }
+
+  LINFO << "Robot navigation completed";
   return 0;
 }
